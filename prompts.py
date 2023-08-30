673508463e34620a8623398ec3b4fe5392df2ca6
A Chethan Reddy <69640722+chethanreddy123@users.noreply.github.com>
2023-08-31 01:07:29 +0530

    added

--- prompts.py
+++ prompts.py
@@ -1,6 +1,7 @@
 def getAnalysisPrompt(query, listOfCompanies):
     prompt = f''' Give below is query from the user on list of companies with detail of each company mentioned.
-    Analysis the details and answer the query with a small descriptive explanation of the analysis.
+    Analysis the details and answer the query with a small descriptive explanation of the analysis .
+    Note: Make sure that the explanation is not too long and is to the point.
     Query : {query}
     List of companies : {listOfCompanies}
     '''
